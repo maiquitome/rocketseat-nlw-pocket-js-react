e729a7f32cac8a4bcd16baaa982b64fa58f1509b
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-13 18:25:37 -0300

    feat: :sparkles: add fetch summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from 'react'
+
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
 
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
+
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setSummary(data.summary)
+      })
+  }, [])
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {summary?.total && summary.total > 20 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
